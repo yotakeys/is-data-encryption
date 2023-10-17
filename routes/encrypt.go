f87851dace3f2d379ea99956a970e4a33c46ee5d
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-10-17 12:11:54 +0700

    refactor: gititgnore upload

--- routes/encrypt.go
+++ routes/encrypt.go
@@ -11,7 +11,7 @@ import (
 func EncryptRoutes(router *gin.Engine, EncryptController controller.EncryptController, jwtService service.JWTService) {
 	encryptRoutes := router.Group("/api/encrypt")
 	{
-		encryptRoutes.POST("", EncryptController.CreateEncrypt)
+		encryptRoutes.POST("", middleware.Authenticate(jwtService), EncryptController.CreateEncrypt)
 		encryptRoutes.GET("", middleware.Authenticate(jwtService), EncryptController.GetAllEncrypt)
 	}
 }
